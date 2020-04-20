79f1e98f936fe724c2a1c270d1b0cb6c9002b086
David Ribeirete Santos <davidribsantos@hotmail.com>
2020-04-20 17:44:21 +0100

    @done
    Signed-off-by: David Ribeirete Santos <davidribsantos@hotmail.com>

---
+++ eyv_app_vue/src/views/welcome.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <v-card   class="mx-auto" fill-height
+    >
+
+        <v-img src="@/assets/background.jpg"      max-height="800"  align="center"   class="white--text  align-center "  >
+
+            <v-card-title class="display-4 py-3 justify-center flex  font-weight-black">Welcome to Eat Your Veggies</v-card-title>
+
+                <v-btn  light to="/login" x-large>Login</v-btn>
+
+                <v-btn  light to="/singup" x-large>SingUp</v-btn>
+
+
+
+
+        </v-img>
+    </v-card>
+
+</template>
