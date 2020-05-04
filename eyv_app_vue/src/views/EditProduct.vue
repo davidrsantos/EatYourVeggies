72c29c47073f54cbd11d100b932882d9e5aab8b1
LuanaNalon <2162359@my.ipleiria.pt>
2020-05-04 18:02:54 +0100

    transfer ownership - i see users

--- eyv_app_vue/src/views/EditProduct.vue
+++ eyv_app_vue/src/views/EditProduct.vue
@@ -102,15 +102,27 @@
                 </v-card>
             </v-col>
         </v-row>
-        <v-dialog v-model="showDialogTransfer">
-            <v-card>
+        <v-dialog v-model="showDialogTransfer" max-width="600">
+            <v-card fluid>
                 <v-select
                         :items="users"
                         item-text="name"
-                        item-value="name"
                         label="Select User"
+                        item-value="publicKey"
+                        return-object
+                        v-model="publicKey"
                         outlined
                 ></v-select>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                            @click="cancel"
+                            color="grey darken-1"
+                    >
+                        Cancel
+                    </v-btn>
+                    <v-btn @click="submit" color="green darken-1" class="mr-4 ml-4">Submit</v-btn>
+                </v-card-actions>
             </v-card>
         </v-dialog>
     </v-container>
@@ -119,6 +131,8 @@
 
 <script>
     import {getPropertyValue} from "../utils/records";
+    const payloads = require('../services/payloads');
+    const transactions = require('../services/transactions');
 
     export default {
         data: () => ({
@@ -134,8 +148,11 @@
                 harvestDate: '',
                 expirationDate: '',
                 packingDate: '',
-                users: []
+
             },
+            users: [],
+            role:'owner',
+            publicKey: ''
         }),
         created: function () {
             if (this.$route.params.recordId != null) {
@@ -145,6 +162,7 @@
         },
         methods: {
           getUsers() {
+            //todo @luana falta validar o tipo dos users, tem de ser apenas abaixo do tipo do user atual
               axios.get('/agents').then(response => {
                 this.users = response.data;
 
@@ -161,13 +179,39 @@
             getProduct() {
                 axios.get(`/records/${this.recordId}`).then(response => {
                     this.product = response.data;
-
+                    //this.recordId = product.recordId;//todo ver se é necessário
                     console.log(this.product);
                 })
                     .catch(function (error) {
                         console.log(error);
                     });
             },
+          cancel() {
+            this.showDialogTransfer = false;
+          },
+          submitProposal(recordId, role, publicKey){
+              let transferPayload = payloads.createProposal({
+                recordId: recordId,
+                receivingAgent: publicKey,
+                role: this.roleToEnum(role)
+              })
+            return transactions.submit([transferPayload], true).then(() => {
+              console.log('Successfully submitted proposal')
+            })
+          },
+          roleToEnum(role){
+            if(role='owner'){
+              return payloads.createProposal.enum.OWNER;
+            }else if(role='custodian'){
+              return payloads.createProposal.enum.CUSTODIAN;
+            }else if(role='reporter'){
+              return payloads.createProposal.enum.REPORTER;
+            }
+          },
+          submit() {
+            this.submitProposal(this.recordId, this.role,this.publicKey);
+            this.showDialogTransfer=false;
+          },
         },
         beforeMount: function () {
             this.getProduct();
