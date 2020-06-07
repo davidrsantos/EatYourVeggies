9d1e650b4a054f479e6a7251911dcddbab08b6ad
David Ribeirete Santos <davidribsantos@hotmail.com>
2020-06-07 16:09:38 +0100

    reduce the time from 50 secs to 3 by changing the query
    Signed-off-by: David Ribeirete Santos <davidribsantos@hotmail.com>

--- eyv_app_vue/src/views/ManagerProducts.vue
+++ eyv_app_vue/src/views/ManagerProducts.vue
@@ -2,30 +2,18 @@
     <v-data-table
             :headers="headers"
             :items="products"
-            sort-by="name"
             class="elevation-1"
+            sort-by="name"
+            :loading="loading"
+            loading-text="Loading... Please wait"
     >
-        <template  v-slot:body="{ items }">
-            <tbody>
-            <tr v-for="item in items" :key="item.recordId">
-                <td>{{ item.recordId }}</td>
-                <td>{{ getPropertyValue(item, 'name') }}</td>
-                <td>{{ getPropertyValue(item, 'classification') }}</td>
-                <td>{{ getPropertyValue(item, 'origin') }}</td>
-                <td> <v-btn
-
-                            small
-                            class="mr-2"
-                            :to="'/editProduct/'+item.recordId"
-                    ><v-icon>
-                        mdi-pencil
-                </v-icon>
-                    </v-btn></td>
-            </tr>
-            </tbody>
+        <template v-slot:item.actions="{ item }">
+                    <v-btn :to="'/editProduct/'+item.recordId" class="mr-2" small>
+                        <v-icon>mdi-pencil</v-icon>
+                    </v-btn>
         </template>
         <template v-slot:top>
-            <v-toolbar flat color="white">
+            <v-toolbar color="white" flat>
                 <v-toolbar-title>My Products</v-toolbar-title>
                 <v-divider
                         class="mx-4"
@@ -34,8 +22,8 @@
                 ></v-divider>
                 <v-spacer></v-spacer>
 
-                <router-link :to="{name: 'addProduct'}" v-if="$store.state.user.role!=='customer'" >
-                    <button color="green" id="myButton" class="mb-2">Add Product</button>
+                <router-link :to="{name: 'addProduct'}" v-if="$store.state.user.role!=='customer'">
+                    <button class="mb-2" color="green" id="myButton">Add Product</button>
                 </router-link>
 
             </v-toolbar>
@@ -44,40 +32,50 @@
 </template>
 
 <script>
-    import {getPropertyValue} from "../utils/records";
+  import { getPropertyValue } from '../utils/records'
 
-    export default {
-        data: () => ({
-            headers: [
-                { text: 'Batch', value: 'recordId' },
-                { text: 'Name', value: 'name' },
-                { text: 'Classification', value: 'classification' },
-                { text: 'Origin', value: 'origin' },
-                { text: 'Actions', value: 'actions', sortable: false },
-            ],
-            products: []
-        }),
-        methods: {
-            getPropertyValue(item, prop){
-
-                return getPropertyValue(item,prop)
-    },
-            getProducts() {
-              if(this.$store.state.user.role == "admin"){
-                axios.get('/records').then(response => {
-                  this.products = response.data;
-                  console.log(this.products);
-                }).catch(error=>{this.$emit('errorEvent', error.response.data.error)})
-              }
-                axios.get('/records?owner='+ this.$store.state.user.publicKey).then(response => {
-                    this.products = response.data;
-                    console.log(this.products);
-                }).catch(error=>{this.$emit('errorEvent', error.response.data.error)})
-            },
-
-        },
-        mounted: function() {
-            this.getProducts();
+  export default {
+    name:'ManagerProducts',
+    data: () => ({
+      loading:true,
+      headers: [
+        { text: 'Batch', value: 'recordId' },
+        { text: 'Name', value: 'name' },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+      products: []
+    }),
+    methods: {
+      getPropertyValue (item, prop) {
+        return getPropertyValue(item, prop)
+      },
+      getProducts () {
+        this.loading = true
+        if (this.$store.state.user.role == 'admin') {
+          axios.get('/records').then(response => {
+            this.products = response.data
+            this.loading = false
+            console.log(this.products)
+          }).catch(error => {
+              this.$emit('errorEvent', error.response.data.error)
+              this.loading = false
+            }
+          )
+        }else {
+          axios.get('/records?owner=' + this.$store.state.user.publicKey).then(response => {
+            this.products = response.data
+            this.loading = false
+            console.log(this.products)
+          }).catch(error => {
+            this.$emit('errorEvent', error.response.data.error)
+            this.loading = false
+          })
         }
+      },
+
+    },
+    mounted: function () {
+      this.getProducts()
     }
+  }
 </script>
